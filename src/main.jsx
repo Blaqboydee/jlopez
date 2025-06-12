61b34b3f01791cdc1b45e0075923d5ab862d1f15
Blaqboydee <adeoluwaadegoke005@gmail.com>
2025-06-12 12:39:12 +0100

    fixed basename issue

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import App from './App'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename="/jlopez">
+    <BrowserRouter>
     <App />
     </BrowserRouter>
     
